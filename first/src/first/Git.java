a1bdc5924a3b3754bdb458aefadf8a62de799ff8
achandankere@gmail.com <achandankere@gmail.com>
2022-03-05 11:17:08 +0530

    changes made

--- first/src/first/Git.java
+++ first/src/first/Git.java
@@ -5,7 +5,7 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int i=10;
+		int i=20;
 		int b=20;
 		int sum=i+b;
 		System.out.println(sum);
